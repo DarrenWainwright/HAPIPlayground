ab5e40cd24f9d15100f614eb71f66de6d1fb75e2
DarrenWainwright <DWainwright@buckland.com>
2020-10-02 12:40:22 -0400

    Initial project creation and start of server app

---
+++ server/app.ts
@@ -0,0 +1,47 @@
+import * as Hapi from "Hapi";
+
+export class App {
+  
+
+    server: Hapi.Server;
+    io:any;
+
+    text_expressRunning = "server - server running on port: ";
+    text_socketConnected = "server - socket.io connected";
+
+    constructor (private host: string){
+        this.server = new Hapi.Server({
+            host: host,
+            port: 3000
+        });
+
+        this.io = require('socket.io')(this.server.listener);        
+    }
+
+    start = async() => {
+        try {
+            await this.server.start();
+            await this.init();
+        } catch (error) {
+            throw new Error(error);
+            process.exit(1);
+        }
+    }
+
+
+    init() {
+        console.log(`success on port: ${this.server.info.port}`);
+        this.startSocketIO();
+    }
+
+    startSocketIO() {
+        this.io.on('connection', (socket:any)=>{
+            socket.emit('socket_connected', {status: this.text_socketConnected});
+            socket.on('sendSocketMessage', (data: any)=>{
+                let text_angularMessage = `server - message from Angular: ${data.socketMessage}`;
+                socket.emit('angular_request', {status: text_angularMessage})
+            });
+
+        });
+    }
+}
